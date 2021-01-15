9910e3746e17d97847ed37d02fa6916e0af38e65
2 FAST 4 U <pereyrago@gmail.com>
2021-01-15 09:23:44 -0300

    finalizado

--- client/src/index.js
+++ client/src/index.js
@@ -16,7 +16,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
